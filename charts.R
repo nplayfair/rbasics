58ee647b75891089360ed8a67a35ebe2570f37c4
Nick Playfair <nick@chaosemerald.co.uk>
2018-07-01 13:54:32 +0100

    charts

--- charts.R
+++ charts.R
@@ -2,3 +2,10 @@ setwd("/Users/Nicholas/Documents/Projects/R/")
 bData <- read.csv("brUsers.csv", TRUE, ",")
 head(bData)
 
+#Histogram
+hist(bData$age, main="Ages of Users", ylab="Users", xlab="Ages")
+
+#Scatter plot
+#plot(bData$age, bData$income, ylab="Income", xlab="Age")
+
+#boxplot(bData$age)
